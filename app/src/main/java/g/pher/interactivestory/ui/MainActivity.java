f6f7e81c8be9a229df996194bc5bc0ecc6c774f8
Fer <pher.gm@gmail.com>
2015-01-04 17:47:29 +0100

    clear nameEditText on game restarts

--- app/src/main/java/g/pher/interactivestory/ui/MainActivity.java
+++ app/src/main/java/g/pher/interactivestory/ui/MainActivity.java
@@ -42,6 +42,12 @@ public class MainActivity extends Activity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        mNameField.setText("");
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
