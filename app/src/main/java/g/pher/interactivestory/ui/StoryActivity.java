c4ca5330a3cd05d64541074d5cb8a428f820b05b
Fer <pher.gm@gmail.com>
2015-01-04 01:31:20 +0100

    added loadPage method (load page attributes from Story class and set them to the Layout)

--- app/src/main/java/g/pher/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/g/pher/interactivestory/ui/StoryActivity.java
@@ -2,17 +2,31 @@ package g.pher.interactivestory.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import g.pher.interactivestory.R;
+import g.pher.interactivestory.model.Page;
+import g.pher.interactivestory.model.Story;
 
 
 public class StoryActivity extends Activity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +34,34 @@ public class StoryActivity extends Activity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
-        if(name == null || name.equalsIgnoreCase(""))
+        mName = intent.getStringExtra(getString(R.string.key_name));
+        if(mName == null || mName.equalsIgnoreCase(""))
         {
-            name = "Fernando";
+            mName = "Fernando";
         }
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
+
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
+
+        loadPage(0);
+    }
+
+    private void loadPage(int numberPage){
+        Page page = mStory.getPage(numberPage);
+
+        Drawable drawable = getResources().getDrawable(page.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = page.getText();
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
+
     }
 
 
